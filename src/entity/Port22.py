e8c8c5963ed30a21237d7d70486b5726d15bab0b
M1nh-Duk <100038173+M1nh-Duk@users.noreply.github.com>
2022-11-04 14:56:33 +0700

    Update Port22.py

--- src/entity/Port22.py
+++ src/entity/Port22.py
@@ -1,35 +1,37 @@
 import os
 
 from dotenv import load_dotenv
-
+from utils import functions as util
+import asyncio
 load_dotenv()
 
 
 class port22:
-    newEncodeRootPass = os.getenv('UPDATE_ROOT_ENCODE_PASS')
-    fakeEncodeRootPass = os.getenv('FAKE_PORT22')
-    host = os.getenv('HOST')
+    NEW_ENCODE_ROOT_PASS = "cG9zdHN3aWdnZXI="
+    FAKE_ENCODE_ROOT_PASS = "CryptoHack"
+    host = "antoine@192.168.0.0"
 
     def __init__(self):
         pass
 
     async def loginCheck(self, ctx):
-        await ctx.send('Login to ' + self.host + ' port 22 successfully\n')
+        await ctx.send(util.syntaxHighlight(f"Login to {self.host} 22 successfully\n",""))
         return True
 
     async def listAllFile(self, ctx):
+        await ctx.send(util.syntaxHighlight("1. changedKey.txt\n2. changedKey.text",""))
         await ctx.send(
-            "This is a part of storage system that store the changed password. Hurry and find the new password")
-        await ctx.send('1. changedKey.txt\n2.changedKey.text')
+            util.syntaxHighlight("This is where the changed password is hidden. Hurry and find it",""))
+        
 
     async def cat(self, ctx, fileName: str):
         if fileName == 'changedKey.txt':
-            await ctx.send(self.fakeEncodeRootPass)
+            await ctx.send(embed=util.embedColor(f"{self.FAKE_ENCODE_ROOT_PASS}","","FILE: changedKey.txt"))
         elif fileName == 'changedKey.text':
-            await ctx.send(
-                'The new password use the new encryption call base64, it totally different from ROT13, Please encryt '
-                + 'it \n')
-            await ctx.send(self.newEncodeRootPass)
+            await ctx.send(embed=util.embedColor(f"{self.NEW_ENCODE_ROOT_PASS}","","FILE: changedKey.text"))
+            await asyncio.sleep(3)
+            await ctx.send(util.syntaxHighlight("The new password uses modern encryption called Base64, it is totally different from ROT13. Please encrypt it",""))
+           
         else:
-            await ctx.send("File not found, please try again.")
+            await ctx.send(embed=util.embedColor("- File not found, please try again - ","diff","ERROR"))
         return True
