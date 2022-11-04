cb07db443cb60af8b0b4c8cd8c88a142116e0a93
M1nh-Duk <100038173+M1nh-Duk@users.noreply.github.com>
2022-11-04 14:59:53 +0700

    Update functions.py

--- src/utils/functions.py
+++ src/utils/functions.py
@@ -1,13 +1,12 @@
 import discord
 
-
 def syntaxHighlight(string, type):
-    temp = str("```") + type + "\n" + string + str("```")
+    temp = f"```{type}\n{string}```"
     return temp
 
 
 def embedColor(string, type, titleName):
-    temp = str("```") + type + "\n" + string + str("```")
-    embed = discord.Embed(color=discord.Color.random(), title=titleName)
-    embed.add_field(name=titleName, value=temp)
-    return embed
+    temp = f"```{type}\n{string}```"
+    ret = discord.Embed(color=discord.Colour.random(),title=titleName)
+    ret.add_field(name="----------------",value=temp)
+    return ret
